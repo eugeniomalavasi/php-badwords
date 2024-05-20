6dbed8991745e2ed6e4a5ed702deba32c41318f7
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-20 15:50:46 +0200

    feat: a horrible style

--- index.php
+++ index.php
@@ -4,20 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./scss/general.scss">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jacquard+12&family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
+    <title>Home</title>
 </head>
 
 <body>
-    <form action="result.php" method="get">
-        <label for="censored-word">inserisci la parola da censurare</label>
-        <input type="text" name="censored" id="censored-word"> <br>
-        <br>
-        <label for="text-input">inserisci il testo</label>
-        <textarea name="input_text" id="text-input"></textarea>
-        <br>
-        <br>
-        <button type="submit"> invia </button>
-    </form>
+    <div class="container">
+        <form action="result.php" method="get">
+            <div>
+                <label for="censored-word">inserisci la parola da censurare:</label>
+                <input type="text" name="censored" id="censored-word">
+            </div>
+
+            <div>
+                <label for="text-input">inserisci il testo:</label>
+                <textarea name="input_text" id="text-input"></textarea>
+            </div>
+
+            <button type="submit"> invia </button>
+        </form>
+    </div>
 </body>
 
 </html>
--- result.php
+++ result.php
@@ -9,38 +9,42 @@ $text = $_GET["input_text"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./scss/general.scss">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jacquard+12&family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
     <title>result page</title>
 </head>
 
 <body>
-    <h2>paragrafo originale</h2>
-    <p>
-        <?php
-        echo $text;
-        ?>
-    </p>
-    <p>
-        <?php
-        echo 'il testo inserito è lungo: ' . strlen($text) . ' caratteri';
-        ?>
-    </p>
-
-    <h2>paragrafo censurato</h2>
-    <p>
-        <?php
-        $result_text = str_replace($censored_word, "***", $text);
-        echo $result_text;
-        ?>
-    </p>
-    <p>
-        <?php
-        echo strlen($result_text);
-        ?>
-    </p>
-
-    <form action="index.php">
-        <button type="submit">torna alla home</button>
-    </form>
+    <div class="container">
+        <h2>paragrafo originale</h2>
+        <p>
+            <?php
+            echo $text;
+            ?>
+        </p>
+        <p>
+            <?php
+            echo 'il testo inserito è lungo: ' . strlen($text) . ' caratteri';
+            ?>
+        </p>
+        <h2>paragrafo censurato</h2>
+        <p>
+            <?php
+            $result_text = str_replace($censored_word, "***", $text);
+            echo $result_text;
+            ?>
+        </p>
+        <p>
+            <?php
+            echo strlen($result_text);
+            ?>
+        </p>
+        <form action="index.php">
+            <button type="submit">torna alla home</button>
+        </form>
+    </div>
 
 </body>
 
